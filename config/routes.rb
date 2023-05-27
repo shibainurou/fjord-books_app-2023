571f6408b2175f0e5028803127ecfb2af09c0ede
shibainurou <shibainurou@gmail.com>
2023-05-27 14:59:17 +0900

    i18n対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
+  scope "(:locale)", locale: /en|ja/ do
+    resources :books
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
