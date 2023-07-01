730c50b6fa760d2c765c73b9ba22cad4d6a6f0ec
shibainurou <shibainurou@gmail.com>
2023-07-01 18:40:06 +0900

    このcontollerだけ個別のlocaleになってしまっているのでlocaleを削除 localeはlocale.rbに記載がある。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action do
-    I18n.locale = :ja # Or whatever logic you use to choose.
-  end
-
   def index
     @users = User.order(:id).page(params[:page]).per(1)
   end
