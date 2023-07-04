25ccbe9dfa5bce4f490574c2ae91dc4a84892724
shibainurou <shibainurou@gmail.com>
2023-07-04 22:57:39 +0900

    ヘッダーコメントを元に戻した config.action_mailer.default_url_options個別のコメントではないため

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  # Settings specified here will take precedence over those in config/application.rb.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -39,7 +40,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Set the mail delivery method.
