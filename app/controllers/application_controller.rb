d33ebff2a764210cb3e5e2f0c2ea69b4deba33a9
shibainurou <shibainurou@gmail.com>
2023-07-05 23:53:30 +0900

    サインイン後はデフォルトでrootへ遷移するのでafter_sign_in_path_forのオーバーライドは不要

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(_resource)
-    books_path
-  end
-
   def signed_in_root_path(_resource_or_scope)
     user_path(current_user.id)
   end
