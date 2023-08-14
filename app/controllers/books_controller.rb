a29b8bb50b8e53fa14801d1ab0bb7fa48933e26e
shibainurou <shibainurou@gmail.com>
2023-08-14 19:07:06 +0900

    show#bookでコメントをつけられるように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,11 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1 or /books/1.json
-  def show; end
+  def show
+    @book = Book.find(params[:id])
+    @comments = @book.comments
+    @comment = Comment.new
+  end
 
   # GET /books/new
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_16_081451) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_095446) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_16_081451) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_16_081451) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
+    t.integer "created_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
