f137c9dfe3aa77c73fb359a46b611f3ee5c39442
shibainurou <shibainurou@gmail.com>
2023-08-23 23:02:52 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy, inverse_of: :user
 end
