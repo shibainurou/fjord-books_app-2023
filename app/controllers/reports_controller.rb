c103b61cf928746a4d3e6f84c3d7ac37d38bc75b
shibainurou <shibainurou@gmail.com>
2023-11-10 19:31:00 +0900

    新規レポート作成の作成は、カレントユーザーを使うように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,8 +25,7 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
-    @report.created_user_id = current_user.id
+    @report = current_user.reports.new(report_params)
 
     respond_to do |format|
       if @report.save
