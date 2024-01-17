61d4ca294696b5ebfd4a3fcf56ef9a6367b1c793
shibainurou <shibainurou@gmail.com>
2024-01-17 22:49:11 +0900

    assert_equalの引数の順番が誤っていたため修正 actual, expcted -> expcted, actual

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,27 +5,25 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test 'return true when the user is the target user' do
     report = reports(:one)
-    assert_equal report.editable?(report.user), true
-  end
+    assert_equal true, report.editable?(report.user)
 
-  test 'return false when the user is not the target user' do
     report1 = reports(:one)
     report2 = reports(:two)
-    assert_equal report2.editable?(report1.user), false
+    assert_equal false, report2.editable?(report1.user)
   end
 
   test 'returns the date part of created_at' do
     report = reports(:one)
     today = Time.zone.today
-    assert_equal report.created_on, today.to_date
+    assert_equal today.to_date, report.created_on
   end
 
   test 'save content without mention' do
     report = Report.new(title: 'test', content: 'test')
     report.save
-    assert_equal report.mentioning_reports.count, 0
-    assert_equal reports(:one).mentioned_reports.count, 0
-    assert_equal reports(:two).mentioned_reports.count, 0
+    assert_equal 0, report.mentioning_reports.count
+    assert_equal 0, reports(:one).mentioned_reports.count
+    assert_equal 0, reports(:two).mentioned_reports.count
   end
 
   test 'save content with mention' do
@@ -35,20 +33,20 @@ class ReportTest < ActiveSupport::TestCase
     report_id = reports(:one).id.to_s
     report = user.reports.new(title: 'test', content: "http://localhost:3000/reports/#{report_id}")
     report.save
-    assert_equal report.mentioning_reports.count, 1
-    assert_equal report.mentioning_reports.first, reports(:one)
-    assert_equal reports(:one).mentioned_reports.count, 1
-    assert_equal reports(:one).mentioned_reports.first, report
-    assert_equal reports(:two).mentioned_reports.count, 0
+    assert_equal 1, report.mentioning_reports.count
+    assert_equal reports(:one), report.mentioning_reports.first
+    assert_equal 1, reports(:one).mentioned_reports.count
+    assert_equal report, reports(:one).mentioned_reports.first
+    assert_equal 0, reports(:two).mentioned_reports.count
   end
 
   test 'update content without mention' do
     report = Report.new(title: 'test', content: 'test')
     report.save
     report.update(title: report.title, content: 'test2')
-    assert_equal report.mentioning_reports.count, 0
-    assert_equal reports(:one).mentioned_reports.count, 0
-    assert_equal reports(:two).mentioned_reports.count, 0
+    assert_equal 0, report.mentioning_reports.count
+    assert_equal 0, reports(:one).mentioned_reports.count
+    assert_equal 0, reports(:two).mentioned_reports.count
   end
 
   test 'update content with mention' do
@@ -57,10 +55,10 @@ class ReportTest < ActiveSupport::TestCase
     report = reports(:two)
     report_id = reports(:one).id.to_s
     report.update(title: report.title, content: "http://localhost:3000/reports/#{report_id}")
-    assert_equal report.mentioning_reports.count, 1
-    assert_equal report.mentioning_reports.first, reports(:one)
-    assert_equal reports(:one).mentioned_reports.count, 1
-    assert_equal reports(:one).mentioned_reports.first, report
-    assert_equal reports(:two).mentioned_reports.count, 0
+    assert_equal 1, report.mentioning_reports.count
+    assert_equal reports(:one), report.mentioning_reports.first
+    assert_equal 1, reports(:one).mentioned_reports.count
+    assert_equal report, reports(:one).mentioned_reports.first
+    assert_equal 0, reports(:two).mentioned_reports.count
   end
 end
